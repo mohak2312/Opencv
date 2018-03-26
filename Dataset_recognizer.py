1127324c91c4a43b7859b3c5fdfce7842881c8f8
Mohak <mohak.patel2312@gmail.com>
2018-03-25 20:58:11 -0700

    Update Dataset_recognizer.py

--- Dataset_recognizer.py
+++ Dataset_recognizer.py
@@ -1,35 +1,5 @@
 import cv2
 import numpy as np
-from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
-import time
-
-cert_path = "c:/certs/"
-host = "a2kc9la4cp40qj.iot.us-east-1.amazonaws.com"
-returntopic = "$aws/things/ir2_proj/shadow/return"
-sendtopic = "$aws/things/ir2_proj/shadow/up"
-logtopic = "$aws/things/ir2_proj/log"
-root_cert = cert_path + "root-CA.crt"
-cert_file = cert_path + "ir2_proj.cert.pem"
-key_file = cert_path + "ir2_proj.private.key"
-
-def customCallback(client, userdata, message):
-    print("Received a new message: ")
-    print(message.payload)
-    print("from topic: ")
-    print(message.topic)
-    print("--------------\n\n")
-
-#starting service
-robot = AWSIoTMQTTClient(host)
-robot.configureEndpoint(host, 8883)
-robot.configureCredentials(root_cert, key_file, cert_file)
-
-# AWSIoTMQTTClient connection configuration
-robot.configureAutoReconnectBackoffTime(1, 32, 20)
-robot.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
-robot.configureDrainingFrequency(2)  # Draining: 2 Hz
-robot.configureConnectDisconnectTimeout(10)  # 10 sec
-robot.configureMQTTOperationTimeout(5)  # 5 sec
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read('Face recogization/face_recognizer.xml')
@@ -37,13 +7,8 @@ recognizer.read('Face recogization/face_recognizer.xml')
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 
-cam = cv2.VideoCapture(1)
+cam = cv2.VideoCapture(0)
 
-
-robot.connect()
-a=0
-last_name=''
-last_frame=''
 while True:   
     ret, im =cam.read()
     
@@ -60,19 +25,7 @@ while True:
         lines = f.read().splitlines()
         name=lines[Id-1].split("_")
         print(name)
-        a+=1
-        print(a)
-        if(a==50 ):
-            if(name[0]!=last_name and name[0] == last_frame ):
-                #robot.publish(sendtopic, str(name[0]),1)
-                print('send')
-                last_name=name[0]
 
-            a=0
-        
-
-            
-        last_frame=name[0]  
         cv2.putText(im,str(name[0]),(x,y-3),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2, cv2.LINE_AA)
        
         
@@ -80,6 +33,5 @@ while True:
     if cv2.waitKey(10) & 0xFF==ord('z'):
         break
 
-robot.disconnect()
 cam.release()
 cv2.destroyAllWindows()
